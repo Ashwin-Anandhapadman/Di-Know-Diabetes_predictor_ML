9fab7f157d27a6498842563f1a853c609973ea74
Ashwin Anandhapadman <ashwinap2212@gmail.com>
2025-11-23 17:04:11 +0530

    updating code

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def index():
 def predict():
     if request.method == "POST":
         try:
-            # Get input values from the form
+            
             Insulin = float(request.form.get('Insulin', 0.0))  # Default to 0.0 if None
             DiabetesPedigreeFunction = float(request.form.get('DiabetesPedigreeFunction', 0.0))
             Pregnancies = float(request.form.get('Pregnancies', 0.0))
@@ -32,34 +32,35 @@ def predict():
             BMI = float(request.form.get('BMI', 0.0))
             BloodPressure = float(request.form.get('BloodPressure', 0.0))
             Glucose = float(request.form.get('Glucose', 0.0))
+            SkinThickness = float(request.form.get('SkinThickness', 0.0))
 
-            # Prepare data for prediction (reshape into 2D array)
-            new_data = np.array([[Insulin, DiabetesPedigreeFunction, Pregnancies, Age, BMI, BloodPressure, Glucose]])
+
+
+            new_data = np.array([[Insulin, DiabetesPedigreeFunction, Pregnancies, Age, BMI, BloodPressure, Glucose, SkinThickness]])
             new_data_scaled = scaler_model.transform(new_data)
 
-            # Predict using all models
+      
             ann_prediction = ann_model.predict(new_data_scaled)
             svm_prediction = svm_model.predict(new_data_scaled)
-            lr_prediction = lr_model.predict(new_data_scaled)
-            xgb_prediction = xgb_model.predict_proba(new_data_scaled)[:, 1]  # Get probabilities for the positive class
+            lr_prediction = lr_model.predict_proba(new_data_scaled)[:, 1] 
+            xgb_prediction = xgb_model.predict_proba(new_data_scaled)[:, 1]  
 
-            # Convert predictions to binary outcomes (e.g., 0 or 1) for voting
+       
             ann_result = (ann_prediction > 0.55).astype(int)   
             lr_result = (lr_prediction > 0.43).astype(int)  
             xgb_result = (xgb_prediction > 0.50).astype(int)  
             svm_result = svm_prediction.astype(int)
 
-            # Aggregate results using majority voting
             final_prediction = np.array([ann_result[0][0], svm_result[0], lr_result[0], xgb_result[0]])
             vote_count = np.bincount(final_prediction.astype(int))
             max_vote_class = np.argmax(vote_count)  # Get class with maximum votes
 
-            # Map numeric result to string
+          
             result_label = "Diabetic !" if max_vote_class == 1 else "Non-Diabetic !"
 
             return render_template('home_diab.html', results=result_label)
         except Exception as e:
-            # Log the error and display a user-friendly message
+
             print(f"Error: {e}")
             return render_template('home_diab.html', results="An error occurred during prediction. Please try again.")
     else:
